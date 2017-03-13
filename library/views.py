8fa4a38378c91e03c71d09cbe22dba6d6a1e21c8
egepsihora <zeland18@ukr.net>
2017-03-13 13:02:02 +0200

    new fonts

--- library/views.py
+++ library/views.py
@@ -1,7 +1,14 @@
-from django.shortcuts import render
+# -*- coding: utf-8 -*-
 
+from django.shortcuts import render
 from django.http import HttpResponse
 
 
+
 def index(request):
-    return render(request, 'library/library.html')
+    contacts = {'city': 'г. Харьков',
+                'street': 'ул. 23 Августа',
+                'phone': '0 800 000 00 00'}
+
+    return render(request, 'library/library.html',
+                  {'contacts': contacts})
