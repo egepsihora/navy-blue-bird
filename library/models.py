a1533b2999e066cbc656e6dd92dd9c68d69cc1bb
egepsihora <zeland18@ukr.net>
2017-03-15 18:23:20 +0200

    add PostgreSQL base, create models

--- library/models.py
+++ library/models.py
@@ -1,5 +1,40 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
 
-# Create your models here.
+@python_2_unicode_compatible
+class Author(models.Model):
+    name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    info = models.TextField(blank=True, default='')
+    books = models.ManyToManyField('Book')
+
+    @property
+    def full_name(self):
+        return self.name + ' ' + self.last_name
+
+    def __str__(self):
+        return self.full_name
+
+
+@python_2_unicode_compatible
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    title = models.CharField(max_length=100)
+    text = models.TextField()
+    commentator = models.ForeignKey('users.User',
+                                    on_delete=models.SET('Anonymous'))
+    book = models.ForeignKey('Book', on_delete=models.CASCADE)
+
+
+
+
+
+
--- users/models.py
+++ users/models.py
@@ -1,5 +1,19 @@
 from __future__ import unicode_literals
-
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+@python_2_unicode_compatible
+class User(AbstractUser):
+    birthDate = models.DateField(null=True, blank=True)
+    index = models.IntegerField(null=True)
+    country = models.CharField(max_length=50, blank=True, default='')
+    city = models.CharField(max_length=20, blank=True, default='')
+    street = models.CharField(max_length=40, blank=True, default='')
+    house = models.IntegerField(null=True)
+    apartments = models.IntegerField(null=True)
+    phone = models.IntegerField(null=True)
+
+    def __str__(self):
+        return self.username
